e5a2d15a4062274ce19d833e20166b54706b895c
DiegoG1019 <dagarciam1014@gmail.com>
2020-03-25 23:25:23 -0400

    Alpha-0.0.7.2

--- Code/DnDT.cs
+++ Code/DnDT.cs
@@ -19,7 +19,7 @@ namespace DnDTools{
 
     class App{
 
-        public static Version version = new Version("Alpha",0,0,7,1);
+        public static Version version = new Version("Alpha",0,0,7,2);
         public const string author = "Diego Garcia";
        
         static void Main(string[] args){
@@ -48,12 +48,12 @@ namespace DnDTools{
             }
 
             string tc = "---------------- \n {0}'s significant skills: {1}";
-            string tcs = "------***------ \n {0} Skill #{1} \n  {2} \n  Key Statistic: {3} \n  Levels: {4} \n  Misc. Levels: {5} \n  Can it be used without training? {6} \n  Is it penalized by armor? {7} \n  Is it a class skill? {8} \n  Skill modifier: {9}";
+            string tcs = "------***------ \n {0} Skill #{1} \n  Key Statistic: {2} \n  Levels: {3} \n  Misc. Levels: {4} \n  Can it be used without training? {5} \n  Is it penalized by armor? {6} \n  Is it a class skill? {7} \n  Skill modifier: {8}";
 
             Console.WriteLine(
                 tc,
                 tchar.desc.name,
-                tcsk.TrimEnd(',')
+                tcsk.Substring(0,tcsk.Length-2)
             );
 
             for(int i = 0; i<tchar.skills.Count; i++){
