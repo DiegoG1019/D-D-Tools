9ec3877d07f5361d5aac5a5d188a8fde76bf239e
DiegoG1016 <dagarciam1014@gmail.com>
2020-10-14 18:50:28 -0400

    -Designed and implemented LabeledNumericBox

---
+++ DMTools/GUI/Elements/Components/LabeledNumericBox.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DiegoG.DnDTDesktop.GUI.Elements.Components
+{
+    partial class LabeledNumericBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Numeric = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Label.Location = new System.Drawing.Point(3, 26);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(144, 124);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "label1";
+            this.Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Label, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Numeric, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(150, 150);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Numeric
+            // 
+            this.Numeric.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Numeric.Location = new System.Drawing.Point(3, 3);
+            this.Numeric.Name = "Numeric";
+            this.Numeric.Size = new System.Drawing.Size(144, 20);
+            this.Numeric.TabIndex = 1;
+            // 
+            // LabeledNumericBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LabeledNumericBox";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Numeric)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.NumericUpDown Numeric;
+    }
+}
---
+++ DMTools/GUI/Elements/Components/LabeledNumericBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiegoG.DnDTDesktop.GUI.Elements.Components
+{
+    public partial class LabeledNumericBox : UserControl
+    {
+        public HorizontalAlignment NumericTextAlignment
+        {
+            get => Numeric.TextAlign;
+            set => Numeric.TextAlign = value;
+        }
+        public string LabelText
+        {
+            get => Label.Text;
+            set => Label.Text = value;
+        }
+        public int NumericValue
+        {
+            get => (int)Numeric.Value;
+            set => Numeric.Value = value;
+        }
+        public LabeledNumericBox()
+        {
+            Numeric.Maximum = int.MaxValue;
+            InitializeComponent();
+        }
+    }
+}
